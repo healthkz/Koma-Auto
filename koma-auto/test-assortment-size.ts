392511aa4856411d602101abd2f11a2172901485
healthcaring <167119383+healthkz@users.noreply.github.com>
2026-06-15 01:36:54 +0500

    Deleting old files
    Deleting

--- koma-auto/test-assortment-size.ts
+++
@@ -1,6 +0,0 @@
-import { fetchMoySklad } from './lib/moysklad';
-async function run() {
-  const res = await fetchMoySklad<any>('/entity/assortment?limit=1');
-  console.log('Total items in assortment:', res.meta.size);
-}
-run();
